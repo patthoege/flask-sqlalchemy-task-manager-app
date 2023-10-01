72ab24d8337e2036c978ca8bc9b7723d810302d2
Patt Höge <paty.hoge98@gmail.com>
2023-10-01 11:24:50 +0000

    add edit_task template and add collapsible for the tasks and and list the task in the routes.py

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
-
     // datepicker initialization
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
@@ -14,6 +13,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
 
 
